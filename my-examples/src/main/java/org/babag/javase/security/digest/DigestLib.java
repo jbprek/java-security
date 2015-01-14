019a80d1992db9fdb741fd3106213c65167c095e
John V. Prekezes <jbprek@gmail.com>
2015-01-14 18:07:31 +0200

    Initial Import

---
+++ my-examples/src/main/java/org/babag/javase/security/digest/DigestLib.java
@@ -0,0 +1,15 @@
+package org.babag.javase.security.digest;
+
+/**
+ * @author prekezes.
+ */
+public class DigestLib {
+
+    public static byte[] hash(String algorithm, String message) {
+
+    }
+
+    public static byte[] hashString(String algorithm, String message) {
+
+    }
+}
